8a4b9b7897e2e492eb15121069d9caad1476d763
dlakic <Daniel.Lakic@mgb.ch>
2019-06-28 16:45:32 +0200

    Fix tests
    This commit updates the snapshots and adds the babel polyfill to the
    testutils, which is used in each test. This is needed since the actions
    now use async await instead of promises.

--- test/testutils.jsx
+++ test/testutils.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider as ReduxProvider } from 'react-redux';
 import configureStore from '../src/client/store';
+import '@babel/polyfill';
 
 export const Provider = ({ children }) => {
   const store = configureStore();
